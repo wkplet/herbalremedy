4a85c9ccb48e242c5067ca5b30778170523de58f
MaurcioGarc1a <maoricio07@gmail.com>
2016-02-06 16:19:47 +0100

    css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+
+    root 'oils#index'
+    get 'contact/index' => 'contact#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'oils#index'
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
